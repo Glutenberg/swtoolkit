74055ecc6fa9c1acd23c795b22aaa2f977c14090
Josh <jdbcoles.eit@gmail.com>
2020-11-11 20:59:17 -0500

    commented out swtoolkit from conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
-import swtoolkit
+# import swtoolkit
 import sphinx_rtd_theme
 
 # -- General configuration ---------------------------------------------
