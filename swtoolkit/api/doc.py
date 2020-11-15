9ad42282d10063af1da2e0fab762a02922c2a938
Josh <jdbcoles.eit@gmail.com>
2020-11-15 11:14:07 -0500

    Factory object implemented

--- swtoolkit/api/doc.py
+++ swtoolkit/api/doc.py
@@ -1,23 +1,36 @@
 from .assemblydoc import AssemblyDoc
-from .partdoc import PartDoc
+from .com import COM
 from .drawingdoc import DrawingDoc
 from .enums.enum_options import DocumentTypes
+from .partdoc import PartDoc
 
 
 class Doc:
+    class __Doc:
+        def __init__(self, system_object=None):
+            self.system_object = system_object
+            del self
+
+        @property
+        def _instance(self):
+            if self.system_object is None:
+                self.system_object = COM("SldWorks.Application").ActiveDoc
+            return self.system_object
+
+        @property
+        def _doc_type(self):
+            return self._instance.GetType
+
+        @property
+        def doc(self):
+            if self._doc_type == DocumentTypes.ASSEMBLY.value:
+                return AssemblyDoc(self._instance)
+            elif self._doc_type == DocumentTypes.PART.value:
+                return PartDoc(self._instance)
+            elif self._doc_type == DocumentTypes.DRAWING.value:
+                return DrawingDoc(self._instance)
+            else:
+                raise ValueError(self._doc_type)
+
     def __new__(cls, system_object=None):
-        object_type = cls._get_type(system_object)
-        return cls._get_object(system_object, object_type)
-
-    def _get_type(system_object):
-        return system_object.GetType
-
-    def _get_object(system_object, object_type: int):
-        if object_type == DocumentTypes.ASSEMBLY.value:
-            return AssemblyDoc(system_object)
-        elif object_type == DocumentTypes.PART.value:
-            return PartDoc(system_object)
-        elif object_type == DocumentTypes.DRAWING.value:
-            return DrawingDoc(system_object)
-        else:
-            raise ValueError(object_type)
+        return cls.__Doc(system_object).doc
